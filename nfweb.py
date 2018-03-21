68bcfdaebd2c4737a2a8f92455fc5f77ce4af14e
Denis Volk <denis.volk@gmail.com>
2018-03-21 18:45:36 +0000

    user logins

--- nfweb.py
+++ nfweb.py
@@ -6,15 +6,52 @@ import shlex
 import os
 import signal
 
-from flask import Flask, request, render_template, redirect, abort
+from flask import Flask, request, render_template, redirect, abort, url_for
+import flask_login
 
 import nflib
 import config
 
 app = Flask(__name__)
+app.secret_key = 'secret key'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = '/login'
 
-cfg = None
-flows = None
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(username):
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+
+    user.is_authenticated = request.form['password'] == users[username]['password']
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.template')
+    if request.method == 'POST':
+        username = request.form['username']
+        if username in users and request.form['password'] == users[username]['password']:
+            user = User()
+            user.id = username
+            flask_login.login_user(user)
+            return redirect('/')
+        return redirect('/login')
 
 def reload_cfg():
     global cfg
@@ -22,13 +59,23 @@ def reload_cfg():
     cfg.load("config.yaml")
     global flows
     flows = dict()
-    for x in cfg.get('nextflows'):
-        flows[x['name']] = x
+    for f in cfg.get('nextflows'):
+        flows[f['name']] = f
+    global users
+    users = dict()
+    for u in cfg.get('users'):
+        users[u['name']] = u
 
 reload_cfg()
 
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
+        
 # todo move this and similar to nflib.py
-@app.route('/status')
+@app.route('/')
+@flask_login.login_required
 def status():
     running = list()
     for _,flow in flows.items():
@@ -40,8 +87,24 @@ def status():
     print(running)
     return render_template('status.template', running=running)
 
+@app.route('/userinfo/<username>')
+@flask_login.login_required
+def userinfo(username: str):
+    is_admin = 'admin' in users[flask_login.current_user.id]['capabilities']
+    is_same = username == flask_login.current_user.id
+    username_exists = username in users
+
+#    if not is_admin or not is_same or not username_exists:
+#        return redirect('/')
+#    else:
+    return render_template('userinfo.template', userinfo=users[username])
+
 @app.route('/admin', methods=['GET', 'POST'])
+@flask_login.login_required
 def admin():
+    if 'admin' not in users[flask_login.current_user.id]['capabilities']:
+        return redirect('/')
+    
     if request.method == 'GET':
         return render_template('admin.template', config_yaml=open("config.yaml").read())
     if request.method == 'POST':
@@ -71,11 +134,13 @@ def admin():
 
         return redirect("/admin")
 
-@app.route('/')
+@app.route('/flows')
+@flask_login.login_required
 def list_flows():
     return render_template('list_flows.template', flows=cfg.get('nextflows'))
 
 @app.route('/flow/<flow_name>/new', methods=['GET', 'POST'])
+@flask_login.login_required
 def begin_run(flow_name: str):
     if request.method == 'GET':
         flow_cfg = flows[flow_name]
@@ -107,6 +172,7 @@ def begin_run(flow_name: str):
         return redirect("/flow/{0}".format(flow_name))
 
 @app.route('/list_all')
+@flask_login.login_required
 def list_all_runs():
     data = list()
     for flow in flows:
@@ -118,6 +184,7 @@ def list_all_runs():
     return render_template('list_runs.template', data=data)
 
 @app.route('/flow/<flow_name>')
+@flask_login.login_required
 def list_runs(flow_name : str):
     data = list()
     try:
@@ -132,6 +199,7 @@ def list_runs(flow_name : str):
     return render_template('list_runs.template', data=data)
 
 @app.route('/flow/<flow_name>/details/<run_uuid>')
+@flask_login.login_required
 def run_details(flow_name : str, run_uuid: int):
     nf_directory = pathlib.Path(flows[flow_name]['directory'])
 
@@ -162,6 +230,7 @@ def run_details(flow_name : str, run_uuid: int):
     return render_template('run_details.template', uuid=run_uuid, flow_name=flow_name, entries=trace_nt, buttons=buttons)
 
 @app.route('/flow/<flow_name>/log/<run_uuid>')
+@flask_login.login_required
 def show_log(flow_name : str, run_uuid: int):
     log_filename = pathlib.Path(flows[flow_name]['directory']) / 'maps' / run_uuid / '.nextflow.log'
     content = None
@@ -171,24 +240,28 @@ def show_log(flow_name : str, run_uuid: int):
     return render_template('show_log.template', content=content, flow_name=flow_name, uuid=run_uuid)
 
 @app.route('/flow/<flow_name>/report/<run_uuid>')
+@flask_login.login_required
 def show_report(flow_name : str, run_uuid: int):
     report_filename = pathlib.Path(flows[flow_name]['directory']) / 'maps' / run_uuid / 'report.html'
     with open(str(report_filename)) as f:
         return f.read()
 
 @app.route('/flow/<flow_name>/timeline/<run_uuid>')
+@flask_login.login_required
 def show_timeline(flow_name: str, run_uuid: int):
     timeline_filename = pathlib.Path(flows[flow_name]['directory']) / 'maps' / run_uuid / 'timeline.html'
     with open(str(timeline_filename)) as f:
         return f.read()
 
 @app.route('/flow/<flow_name>/dagdot/<run_uuid>')
+@flask_login.login_required
 def show_dagdot(flow_name: str, run_uuid: int):
     dagdot_filename = pathlib.Path(flows[flow_name]['directory']) / 'maps' / run_uuid / 'dag.dot'
     with open(str(dagdot_filename)) as f:
         return f.read()
 
 @app.route('/flow/<flow_name>/stop/<run_uuid>')
+@flask_login.login_required
 def kill_nextflow(flow_name : str, run_uuid: int):
     pid_filename = pathlib.Path(flows[flow_name]['directory']) / 'pids' / pathlib.Path("{0}.pid".format(run_uuid)).name
     if pid_filename.is_file():
